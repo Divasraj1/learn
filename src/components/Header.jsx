5a70fd7b9d97347467afd8fca77c31c4920dee0b
Divasraj Vimal <vimaldivas12@gmail.com>
2022-04-12 19:28:23 +0530

    Header Complete

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import ForumOutlinedIcon from '@mui/icons-material/ForumOutlined';
+import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+
+import "../styles/Header.css"
+function header() {
+  return (
+    <div className='header'>
+        <div className='title'>
+            <h2>Learn</h2>
+        </div>
+        <div className='search-bar'>
+        <div className='search-bar-container'>
+            <SearchOutlinedIcon />
+            <input type="text" placeholder='Search'/>
+        </div>
+        </div>
+        <div className='menu-items'>
+            <div className='profile header-icon'>
+                <AccountCircleOutlinedIcon/>
+            </div>
+            <div className='messages header-icon'>
+                <ForumOutlinedIcon/>
+            </div>
+            <div className='badges-acheivements header-icon'>
+                <EmojiEventsOutlinedIcon />
+            </div>
+            <div className='help header-icon'>
+                <HelpOutlineOutlinedIcon />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default header
