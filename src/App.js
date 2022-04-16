ef0abcf90d157c3adf28a060f6c445478e222e0e
NIKHILMALKHEDE <malkhedenikhil@gmail.com>
2022-04-16 23:56:07 +0530

    UI changes andded Question

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import './App.css';
-import Header from "./components/Header"
-import Question from './components/Question';
-import Sidebar from './components/Sidebar';
-import EliteMentor from './components/EliteMentor';
+import "./App.css";
+import Header from "./components/Header";
+import Question from "./components/Question";
+import Sidebar from "./components/Sidebar";
+import EliteMentor from "./components/EliteMentor";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <EliteMentor/>
-      <div style={{display:"flex"}}>
-        <Sidebar/>
-          <div>
-            <Question/>
-            <Question/>
-            <Question/>
-            <Question/>
-          </div>
+      <EliteMentor />
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+        <div>
+          <Question
+            title="Linear motion of Moving Train"
+            summary=" A train is moving along a straight line with a constant acceleration ‘a’. A girl standing in the train throws a ball forward with a speed of 10 m/s, at an angle of 600 to the horizontal. The girl has to move forward by 1.15 m inside the train to catch the ball back at the initial height. The acceleration of the train, in m/s2, is"
+            tag1="physics"
+            tag2="motion"
+            tag3="Linear motion"
+            tag4="Acceleration"
+            username="Nikhil"
+          />
+          <Question
+            title=" Adiabatic temperature transfer"
+            summary="The work of 146 kJ is performed in order to compress one-kilo mole of gas adiabatically and in this process the temperature of the gas increases by 7ºC. The gas is (R = 8.3 J mol–1 K–1)"
+            tag1="physics"
+            tag2="Thermodynamics"
+            tag3="Adiabatically"
+            tag4="temperature"
+            username="Divas"
+          />
+          <Question
+            title="Change in internal energy "
+            summary="Two moles of an ideal monatomic gas occupies a volume V at 27°C. The gas expands adiabatically to a volume 2V. Calculate (i) the final temperature of the gas and (ii) change in its internal energy."
+            tag1="physics"
+            tag2="Thermodynamics"
+            tag3="Monatomic"
+            tag4="Internal energy"
+            username="Hritik"
+          />
+          <Question
+            title="Ration of two magnetic field"
+            summary="A magnetic compass needle oscillates 30 times per minute at a place where the dip is 45°, and 40 times per minute where the dip is 30°. If B1 and B2 are respectively the total magnetic field due to the earth at the two places, then the ratio B1 /B2 is best given by"
+            tag1="physics"
+            tag2="Magnetism"
+            tag3="Magnetic field"
+            tag4="Flux"
+            username="Amit"
+          />
+          <Question
+            title="How to calculate Coefficient of friction "
+            summary="A smooth block is released at rest on a 450 incline and then slides a distance d. The time taken to slide is n times as much to slide on a rough incline than on a smooth incline. The coefficient of friction is-"
+            tag1="physics"
+            tag2="Friction"
+            tag3="Coefficient of friction"
+            tag4="Force"
+            username="Yatharth"
+          />
+          <Question
+            title="Incline friction motion"
+            summary="The upper half of an inclined plane with inclination Φ is perfectly smooth, while the lower half is rough. A body starting from rest at the top will again come to rest at the bottom, if the coefficient of friction for the lower half is given by"
+            tag1="physics"
+            tag2="Friction"
+            tag3="Coefficient of frictione"
+            tag4="Force"
+            username="Shreyansh"
+          />
+          <Question
+            title="Force and Motion "
+            summary="The minimum force required to start pushing a body up a rough (frictional coefficient μ) inclined plane F1 while the minimum force needed to prevent it from sliding down is F2. If the inclined plane makes an angle θ from the horizontal such that tan θ= 2μ then the ratio F1/F2 is"
+            tag1="physics"
+            tag2="motion"
+            tag3="Force"
+            tag4="friction"
+            username="Shivam"
+          />
         </div>
+      </div>
     </div>
   );
 }
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../styles/Question.css'
 
-const Question = () => {
+const Question = (props) => {
   return (
     <div className='Section'>
        <div className='Rating'>
@@ -10,17 +10,17 @@ const Question = () => {
           <span>2 Views</span>
        </div>
        <div className='Question'>
-          <p className='Question-title'>Store GPX file into MYSQL database in Java</p>
-          <p className='Question-summary'> I'm trying to store the contents of a GPX file into a My SQL database as part of a Java Spring route sharing application I'm working on. I will use this data to generate maps displaying routes within</p>
+          <p className='Question-title'>{props.title}</p>
+          <p className='Question-summary'>{props.summary}</p>
           <div className='Question-bottom'>
              <div className='Tags'>
-               <span className='Tag'>Java</span>
-               <span className='Tag'>MySql</span>
-               <span className='Tag'>Spring</span>
-               <span className='Tag'>Gpx</span>
+               <span className='Tag'>{props.tag1}</span>
+               <span className='Tag'>{props.tag2}</span>
+               <span className='Tag'>{props.tag3}</span>
+               <span className='Tag'>{props.tag4}</span>
              </div>
              <div className='Info'>
-                <p className='Username'>Laughing Coffin </p>
+                <p className='Username'>{props.username} </p>
                 <p className='Time'><strong>29 </strong> asked 23 min ago</p>
              </div>
           </div>
